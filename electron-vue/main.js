e434efd1bd7004dbfbb4464a3bd47f421a291d49
Gabby Strevay <gstrevay@gmail.com>
2025-04-15 22:54:36 -0700

    updated calendar to remove toggle weekend option, unnecessary and added alert to show a popup for date/event clicked

--- electron-vue/main.js
+++ electron-vue/main.js
@@ -21,14 +21,11 @@ function createWindow() {
       slashes: true
     })
   );
-
-  mainWindow.webContents.once('dom-ready', () => {
-    mainWindow.webContents.openDevTools(); // Open DevTools once DOM is ready
-  });
-  
+  /*
   mainWindow.on('closed', function () {
     mainWindow = null
   })
+  */
 }
 console.log(app);
 app.on('ready', createWindow)
--- electron-vue/src/views/CalendarView.vue
+++ electron-vue/src/views/CalendarView.vue
@@ -12,18 +12,27 @@ export default {
       calendarOptions: {
         plugins: [ dayGridPlugin, interactionPlugin ],
         initialView: 'dayGridMonth',
-        weekends: false // initial value
+        dateClick: this.handleDateClick,
+        eventClick: this.handleEventClick,
+        events: [
+          { title: 'event 1', date: '2025-04-01' },
+          { title: 'event 2', date: '2025-04-23' }
+        ]
       }
     }
   },
   methods: {
-    toggleWeekends: function() {
-      this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+    handleDateClick: function(arg) {
+      alert('date click! ' + arg.dateStr)
+    },
+    handleEventClick(info) {
+      const date = info.event.start
+      const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+      alert(`event click!\nTitle: ${info.event.title}\nDate: ${formattedDate}`)
     }
   }
 }
 </script>
 <template>
-  <button @click="toggleWeekends">toggle weekends</button>
   <FullCalendar :options="calendarOptions" />
 </template>
